5666bfe2f844589afc9fd5f857481fe45833b201
Tolu Joel <toluisjoel@gmail.com>
2023-09-06 17:29:46 +0100

    Week 3 Day 2 and 1 updated

--- Week 3/Day 1/2_parameters.py
+++ Week 3/Day 1/2_parameters.py
@@ -1,12 +1,26 @@
+# parameters
+def greeting(name):
+    print(f"Welcome {name}")
+
+
 def greeting(name, age, gender):
     print(f"Welcome {name}, you are {age} years old and you're a {gender}")
 
 
+greeting(gender="female", age=16, name='becky')
+
+
+# default parameters
+
+
+def greeting(name, age, gender="male"):
+    print(f"Welcome {name}, you are {age} years old and you're a {gender}")
+
+
 # positional argument
-greeting("james", 27, "male")
+greeting("james", 27)
 
 # keyword argument
-greeting(gender="female", age=16, name='becky')
 
 # positional and keyword argument
 greeting("john", gender="male", age=91)
