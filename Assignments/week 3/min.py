ed52d10b2d1efb97e5a0bb5f1e149b223028a7bb
email2ify <110549305+email2ify@users.noreply.github.com>
2023-09-07 11:22:33 +0200

    implimentation

--- Assignments/week 3/min.py
+++ Assignments/week 3/min.py
@@ -1,2 +1,9 @@
 # create a function that takes a list as an argument,
 # and returns the minimum(smallest) number in the list
+
+def number(nums):
+    min_num = (1)
+    for num in nums:
+        if num == min_num:
+         print(num)
+number([23,46,4,9,2,1,15])        
