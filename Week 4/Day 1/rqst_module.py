d42cd63658dc766dd2628e1bc6fa26f7d449072c
Tolu Joel <toluisjoel@gmail.com>
2023-09-18 18:11:20 +0100

    Week 4 Day 1 Updated

---
+++ Week 4/Day 1/rqst_module.py
@@ -0,0 +1,24 @@
+# request module
+
+import requests
+
+web_page = requests.get(
+    'https://finelib.com/cities/lagos/business/food/restaurants')
+
+print(web_page.text)
+
+# libraries, packages, modules
+
+# web scraping
+# requests
+# beautifulsoup
+# selenium
+
+# data analysis
+# numpy
+# pandas
+
+
+# web frameworks
+# Django
+# flask
