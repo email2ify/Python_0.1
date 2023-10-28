f03ba67e6c9e2b5af59b4f28e1a0eda1107ccf63
Tolu Joel <toluisjoel@gmail.com>
2023-10-29 00:02:58 +0100

    Add lists assignment

---
+++ Tasks/lists/comma.py
@@ -0,0 +1,8 @@
+# Write a function that takes a list value as an argument and returns
+# a string with all the items separated by a comma and a space, with and inserted before the last item. 
+# But your function should be able to work with any list value passed to it.
+# Be sure to test the case where an empty list [] is passed to your function.
+
+
+# OUTPUT:
+#     Ouputs/comma.png
